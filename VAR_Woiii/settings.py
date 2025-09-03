daa4858a9d1b5f3720d7221d605296dbd7d42fd7
Jaysen Lestari <lestarijaysen@gmail.com>
2025-09-04 02:02:17 +0700

    Fix secret key

--- VAR_Woiii/settings.py
+++ VAR_Woiii/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-y-2-=r1%w1$&#eirhp6_lti%#eoz6f%v9-r17&5wcu-6u^=8td"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
