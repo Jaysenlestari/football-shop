6cc50a55a5130913feb2122becdd3e508235f422
Jaysen Lestari <lestarijaysen@gmail.com>
2025-09-17 23:52:53 +0700

    Finished TI4

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
@@ -26,6 +27,7 @@ class Product(models.Model):
         ('43', '43'),
     ]
     
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=150)
     price = models.IntegerField()
