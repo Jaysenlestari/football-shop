ae708a681face3d5df4ceda12485dc2b3a1286bb
Jaysen Lestari <lestarijaysen@gmail.com>
2025-09-17 23:55:44 +0700

    Fix env for db creds

--- VAR_Woiii/settings.py
+++ VAR_Woiii/settings.py
@@ -102,14 +102,6 @@ else:
         }
     }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
