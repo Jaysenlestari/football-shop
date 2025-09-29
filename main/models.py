6f9a77703abdc53a8b6821e53ed9d38c07067254
Jaysen Lestari <lestarijaysen@gmail.com>
2025-09-29 13:34:10 +0700

    Testing TI5

--- main/models.py
+++ main/models.py
@@ -35,11 +35,11 @@ class Product(models.Model):
     description = models.TextField()
     thumbnail = models.URLField()
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
-    is_featured = models.BooleanField(default=False)
     stock = models.IntegerField()
     rating = models.FloatField(default=0.0)
     clothes_size = models.CharField(max_length=5, choices=CLOTHES_SIZES, blank=True, null=True)
     shoe_size = models.CharField(max_length=5, choices=SHOE_SIZES, blank=True, null=True)
+    is_featured = models.BooleanField(default=False)
     
     def __str__(self):
         return self.name
